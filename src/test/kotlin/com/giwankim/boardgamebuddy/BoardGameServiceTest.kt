06b8afd1478ed45bc2d680c79455ad774878d11c
Giwan Kim <giwan.kim@gmail.com>
2025-09-14 09:25:29 +0900

    test: add fact-checking evaluation test to BoardGameServiceTest
    - Introduced `FactCheckingEvaluator` to BoardGameServiceTest.
    - Added unit test to evaluate factual accuracy of answers provided by the service.

--- src/test/kotlin/com/giwankim/boardgamebuddy/BoardGameServiceTest.kt
+++ src/test/kotlin/com/giwankim/boardgamebuddy/BoardGameServiceTest.kt
@@ -3,6 +3,7 @@ package com.giwankim.boardgamebuddy
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
 import org.springframework.ai.chat.client.ChatClient
+import org.springframework.ai.chat.evaluation.FactCheckingEvaluator
 import org.springframework.ai.chat.evaluation.RelevancyEvaluator
 import org.springframework.ai.evaluation.EvaluationRequest
 import org.springframework.boot.test.context.SpringBootTest
@@ -13,6 +14,8 @@ class BoardGameServiceTest(
     chatClientBuilder: ChatClient.Builder,
 ) {
     val relevancyEvaluator: RelevancyEvaluator = RelevancyEvaluator(chatClientBuilder)
+
+    val factCheckingEvaluator: FactCheckingEvaluator = FactCheckingEvaluator(chatClientBuilder)
 
     @Test
     fun `evaluate relevancy`() {
@@ -30,6 +33,31 @@ class BoardGameServiceTest(
                 ========================================
                 The answer "%s"
                 is not considered relevant to the question
+                "%s".
+                ========================================
+                """.trimIndent(),
+                answer.answer,
+                userText,
+            ).isTrue
+    }
+
+    @Test
+    fun `evaluate factual accuracy`() {
+        val userText = "Why is the sky blue?"
+        val question = Question(userText)
+        val answer = boardGameService.askQuestion(question)
+        val referenceAnswer =
+            "The sky is blue because of that was the paint color that was on sale."
+
+        val evaluationRequest = EvaluationRequest(userText, answer.answer)
+        val evaluationResponse = factCheckingEvaluator.evaluate(evaluationRequest)
+
+        assertThat(evaluationResponse.isPass)
+            .withFailMessage(
+                """
+                ========================================
+                The answer "%s"
+                is not considered correct for the question
                 "%s".
                 ========================================
                 """.trimIndent(),
